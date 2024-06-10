ae9e8c7560cf1e82e93f6cf5d69a75d9b02ef971
Andrei Pshenichnyi <andrei@MacBook-Air-Andrei.local>
2024-06-10 20:08:06 +0400

    Minor improvements added

--- recipes/calculator/views.py
+++ recipes/calculator/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render
 
-DATA = {
-    'omlet': {
-        'яйца, шт': 2,
-        'молоко, л': 0.1,
-        'соль, ч.л.': 0.5,
-    },
-    'pasta': {
-        'макароны, г': 0.3,
-        'сыр, г': 0.05,
-    },
-    'buter': {
-        'хлеб, ломтик': 1,
-        'колбаса, ломтик': 1,
-        'сыр, ломтик': 1,
-        'помидор, ломтик': 1,
-    },
-}
-
 
 def recipes_views(request, dish_name):
+
+    DATA = {
+        'omlet': {
+            'яйца, шт': 2,
+            'молоко, л': 0.1,
+            'соль, ч.л.': 0.5,
+        },
+        'pasta': {
+            'макароны, г': 0.3,
+            'сыр, г': 0.05,
+        },
+        'buter': {
+            'хлеб, ломтик': 1,
+            'колбаса, ломтик': 1,
+            'сыр, ломтик': 1,
+            'помидор, ломтик': 1,
+        },
+    }
+
     number = int(request.GET.get("servings", 1))
 
     dishes_data = DATA
     ingredients = dishes_data[dish_name]
-    
+
     for name, quantaty in ingredients.items():
         ingredients[name] = ingredients[name] * number
 
